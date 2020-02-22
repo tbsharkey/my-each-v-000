5b04209473b035bfca263f51f19170027e06bb61
Tom Sharkey <tbsharkey@gmail.com>
2020-02-22 05:20:43 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,9 @@
 collection = []
-def my_each(collection)  # put argument(s) here
-i = 0
-
-while i < collection.length
-  yield collection[i]
-  i += 1
-
-end
-collection
+def my_each(collection)
+  i = 0
+  while i < collection.length
+    yield collection[i]
+    i += 1
+  end
+  collection
 end
